58f65ccb111db9856c95f2c893d0c2b4b9a9f446
Tim Boisvert <tboisvert@asheavenue.com>
2013-05-24 15:51:43 -0400

    dummy methods to get started

--- lib/Seedr.rb
+++ lib/Seedr.rb
@@ -1,5 +1,17 @@
 require "Seedr/version"
 
 module Seedr
-  # Your code goes here...
+  
+  def self.first_name
+    "Joe"
+  end
+  
+  def self.last_name
+    "Smith"
+  end
+  
+  def self.name
+    "#{self.first_name} #{self.last_name}"
+  end
+  
 end
