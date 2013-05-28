bd033840f3283dd79d11bd55de2f2875abde1f44
Tim Boisvert <tboisvert@asheavenue.com>
2013-05-28 17:31:56 -0400

    changed comapny name spacing

--- lib/Seedr.rb
+++ lib/Seedr.rb
@@ -33,15 +33,16 @@ module Seedr
   def self.company_name
     word1 = ['ACME', 'American', 'International', 'Blue', "Dad's", 'Stargazer', 'East River', 'Nissan', 'Red', 'Standard']
     word2 = ['Computer', 'Plumbing', 'Construction', 'Welding', 'Mercantile', 'Design', 'Refrigeration', 'Shipping', 'Culinary', 'Dry Goods']
-    word3 = ['Company', 'Society', 'Association', 'Union', 'Supply', '', '', '', '', '']
-    word4 = ['of North Carolina', ', Inc.', 'LLC', '', '', '', '', '', '']
-    "#{word1.sample} #{word2.sample} #{word3.sample} #{word4.sample}"
+    word3 = [' Company', ' Society', ' Association', ' Union', ' Supply', '', '', '', '', '']
+    word4 = [' of North Carolina', ', Inc.', ', LLC', '', '', '', '', '', '']
+    "#{word1.sample} #{word2.sample}#{word3.sample}#{word4.sample}"
   end
   
   def self.email
-    username = ['bob', 'horace', 'jimmy', 'info', 'noreply', 'stuff', 'juan', 'daisuke', 'veronica', 'marge', 'inquiries']
+    username = ['bob', 'horace', 'jimmy', 'info', 'noreply', 'stuff', 'juan', 'daisuke', 'veronicgit a', 'marge', 'inquiries']
     host = ['gmail', 'hotmail', 'yahoo', 'nytimes', 'runners', 'hotdating', 'cnn', 'maryjanetimes', 'nyc', 'navy']
     tld = ['com', 'net', 'io', 'gov', 'mil', 'co.uk', 'es']
-    "#{username.sample}@#{host.sample}.#{tld}"
+    "#{username.sample}@#{host.sample}.#{tld.sample}"
   end
+  
 end
